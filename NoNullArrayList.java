bcb694f05900b45ed4a568536242aacdb567cd2a
Kayla Fang <kfang00@stuy.edu>
2018-11-02 14:46:21 -0400

    format

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,48 +1,23 @@
+import java.util.ArrayList;
 public class NoNullArrayList<T> extends ArrayList<T>{
   public NoNullArrayList(){
-
+    super();
   }
 
   public NoNullArrayList(int startingCapacity){
-    value = startingCapacity;
+    super(startingCapacity);
   }
 
   public String set(int b, String c) {
-    String d = data[b];
-    if (b < 0 || b >= size()) {
-      throw new IndexOutOfBoundsException();
-    }
-    data[b] = c;
-    return d;
-}
+    return "";
+  }
 //no sorting just putting restrictions so that u add them in order
   public boolean add(String a) {
-    if (size >= (data.length - 1)) {
-      resize();
-
-    }
-    data[size] = a;
-    size += 1;
     return true;
   }
 
-public void add(int e, String yay) {
-  int track = 0;
-  String[] arrCopy = new String[size() + 1];
-  if ( e < 0 || e > size()) {
-    throw new IndexOutOfBoundsException();
-  }
-  else {
-    for (int x = 0; x < e; x++) {
-      arrCopy[x] = data[x];
-      track += 1;
-    }
-    arrCopy[track] = yay;
-    for (int x = track; x < size() ; x++) {
-      arrCopy[x + 1] = data[x];
-    }
-    data = arrCopy;
-    size += 1;
+  public void add(int e, String yay) {
+
   }
 
 }
