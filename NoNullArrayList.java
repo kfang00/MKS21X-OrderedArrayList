73b68969432a74a8a5e984c8041501caa0b04acd
Kayla Fang <kfang00@stuy.edu>
2018-11-02 14:25:27 -0400

    added previous code

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -11,5 +11,43 @@ public class NoNullArrayList<T> extends ArrayList<T>{
   public T getValue() {
     return value;
   }
-  
+
+  public String set(int b, String c) {
+    String d = data[b];
+    if (b < 0 || b >= size()) {
+      throw new IndexOutOfBoundsException();
+    }
+    data[b] = c;
+    return d;
+}
+
+public boolean add(String a) {
+    if (size >= (data.length - 1)) {
+      resize();
+
+    }
+    data[size] = a;
+    size += 1;
+    return true;
+  }
+
+public void add(int e, String yay) {
+  int track = 0;
+  String[] arrCopy = new String[size() + 1];
+  if ( e < 0 || e > size()) {
+    throw new IndexOutOfBoundsException();
+  }
+  else {
+    for (int x = 0; x < e; x++) {
+      arrCopy[x] = data[x];
+      track += 1;
+    }
+    arrCopy[track] = yay;
+    for (int x = track; x < size() ; x++) {
+      arrCopy[x + 1] = data[x];
+    }
+    data = arrCopy;
+    size += 1;
+  }
+
 }
